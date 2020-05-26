98a410f63fc61b89a2aa00726ecaf65e6579f2f6
iyer <ptr-64>
2020-05-26 14:43:44 -0400

    fixed 'R' to 'r' in the `pacman` command in dotfiles/install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # peter's bash installer
 
-sudo pacman -S chromium inkscape julia python-pip R ranger zathura zathura-cb zathura-djvu zathura-pdf-mupdf
+sudo pacman -S chromium inkscape julia python-pip r ranger zathura zathura-cb zathura-djvu zathura-pdf-mupdf
 
 for file in ~/dotfiles/{path, bash_prompt, exports, aliases, functions, extra}; do
-	[-r "$file ] && [ -f "$file" ] && ln -s ~/dotfiles/"$file" ~/"$file";
+	[-r "$file" ] && [ -f "$file" ] && ln -s ~/dotfiles/"$file" ~/"$file";
 done;
