aefc36f49f270ab310e491780fcee798b5935945
iyer <ptr-64>
2020-05-26 16:43:21 -0400

    added texlive-most and biber packages to the install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # peter's bash installer
 
-sudo pacman -S chromium inkscape julia python-pip r ranger zathura zathura-cb zathura-djvu zathura-pdf-mupdf
+sudo pacman -S biber chromium inkscape julia python-pip r ranger texlive-most zathura zathura-cb zathura-djvu zathura-pdf-mupdf
 
 for file in ~/dotfiles/{path, bash_prompt, exports, aliases, functions, extra}; do
 	[-r "$file" ] && [ -f "$file" ] && ln -s ~/dotfiles/"$file" ~/"$file";
