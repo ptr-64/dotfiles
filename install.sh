a4f4bad0de9e3cda51f2e88367ef6cf8be894d94
ptr-64 <parkeri@bu.edu>
2020-06-01 20:30:22 -0400

    ia few chngekbkbkbanges to the install srkbcriptjk;wq;q;q!

--- install.sh
+++ install.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 # peter's bash installer
 
+# these are the pacman packages I need to have in every work device
 sudo pacman -S biber chromium inkscape julia python-pip r ranger texlive-most zathura zathura-cb zathura-djvu zathura-pdf-mupdf
 
+# Now symlink the relevant dotfiles
 for file in ~/dotfiles/{path, bash_prompt, exports, aliases, functions, extra}; do
 	[-r "$file" ] && [ -f "$file" ] && ln -s ~/dotfiles/"$file" ~/"$file";
 done;
+# This symlink ensures my ultisnips snippets still work with my vimrc.
+ln -s ~/dotfiles/mysnippets ~/.vim/mysnippets
+
+# install my essential python packages
+pip3 install Unidecode virtualenv virtualenvwrapper
