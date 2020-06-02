30e824c4ced2d0306d3dd6a83132b3003d804951
ptr-64 <parkeri@bu.edu>
2020-06-01 21:11:27 -0400

    added w3m to the install script to allow st to render images in ranger

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # peter's bash installer
 
 # these are the pacman packages I need to have in every work device
-sudo pacman -S biber chromium inkscape julia python-pip r ranger texlive-most zathura zathura-cb zathura-djvu zathura-pdf-mupdf
+sudo pacman -S biber chromium inkscape julia python-pip r ranger texlive-most w3m zathura zathura-cb zathura-djvu zathura-pdf-mupdf
 
 # Now symlink the relevant dotfiles
 for file in ~/dotfiles/{path, bash_prompt, exports, aliases, functions, extra}; do
