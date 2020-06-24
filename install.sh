59f259a1c9ac1eb9a4425858291b3116921a69a9
ptr-64 <parkeri@bu.edu>
2020-06-24 14:03:17 -0400

    changes to the install script

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 
 #
 # these are the pacman packages I need to have in every work device
-sudo pacman -Syyu biber chromium inkscape julia kitty newsboat python-pip r ranger texlive-most w3m zathura zathura-cb zathura-djvu zathura-pdf-mupdf
+sudo pacman -Syyu biber chromium inkscape julia kitty mpv newsboat python-pip r ranger texlive-most w3m zathura zathura-cb zathura-djvu zathura-pdf-mupdf
 
 # Now symlink the relevant dotfiles
 for file in ~/dotfiles/{path, bash_prompt, exports, aliases, functions, extra}; do
