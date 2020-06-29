7212d3d680f8d716b57b88a35f10dda24f92647c
ptr-64 <p.iyer@protonmail.com>
2020-06-29 11:17:38 -0400

    The vim snippet symlink needs work, the .vim dir will not exist in /home/iyer/ until plugins have been installed

--- install.sh
+++ install.sh
@@ -3,19 +3,19 @@
 
 #
 # these are the pacman packages I need to have in every work device
-sudo pacman -Syyu biber chromium inkscape julia kitty mpv newsboat python-pip r ranger texlive-most w3m zathura zathura-cb zathura-djvu zathura-pdf-mupdf
+sudo pacman -Syyu biber firefox inkscape julia kitty mpv newsboat python-pip r ranger texlive-most vim zathura zathura-cb zathura-djvu zathura-pdf-mupdf
 
 # Now symlink the relevant dotfiles
-for file in ~/dotfiles/{path, bash_prompt, exports, aliases, functions, extra}; do
+for file in ~/dotfiles/{path, bash_prompt, exports, aliases, functions, extra, vimrc}; do
 	[-r "$file" ] && [ -f "$file" ] && ln -s /home/"$USER"/dotfiles/"$file" /home/"$USER"/"$file";
 done;
-
+# run vim once to allow the plugins to get installed.
 # This symlink ensures my ultisnips snippets still work with my vimrc.
-ln -s /home/"$USER"/dotfiles/mysnippets /home/"$USER"/.vim/mysnippets
+# ln -s /home/"$USER"/dotfiles/mysnippets /home/"$USER"/.vim/mysnippets
 
 # This symlink configures newsboat:
-ln -s /home/"$USER"/dotfiles/newsboat/urls /home/"$USER"/.newsboat
-ln -s /home/"$USER"/dotfiles/newsboat/config /home/"$USER"/.newsboat
+ln -s /home/"$USER"/dotfiles/newsboat/urls /home/"$USER"/.newsboat/
+ln -s /home/"$USER"/dotfiles/newsboat/config /home/"$USER"/.newsboat/
 
 # install my essential python packages
 pip3 install Unidecode virtualenv virtualenvwrapper
